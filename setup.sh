020cafd8582eed2c13644c2fc95cfcf933e40b26
Rúben Ferreira <telemovelsic@hotmail.com>
2021-07-04 03:01:14 +0100

    Removed repo cleaning behaviour, from now on you should clean on start.sh if required

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ while true; do
   # Clone the repo
   echo "Cloning $GIT_REPO this may take a while..."
   git -C repo fetch --all
-  git -C repo clean -fdx
+  # git -C repo clean -fdx
   git -C repo reset --hard origin/master || git clone https://"$GITHUB_USER":"$GITHUB_TOKEN"@"$GIT_REPO" repo
 
   # Enter the repo or exit on error
