88f8e6a1b84020362256bd8fac48522f4af8047d
Lily <lily.code99@gmail.com>
2024-12-18 15:32:30 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
-while true; do
-  # Clone the repo
-  echo "Cloning $GIT_REPO this may take a while..."
-  git -C repo fetch --all
-  # git -C repo clean -fdx
-  git -C repo reset --hard origin/master || git clone https://"$GITHUB_USER":"$GITHUB_TOKEN"@"$GIT_REPO" repo
+# Clone the repo
+echo "Cloning $GIT_REPO this may take a while..."
+git -C repo fetch --all
+# git -C repo clean -fdx
+git -C repo reset --hard origin/master || git clone https://"$GITHUB_USER":"$GITHUB_TOKEN"@"$GIT_REPO" repo
 
-  # Enter the repo or exit on error
-  echo "Cloning complete, entering repo directory"
-  cd repo || exit
+# Enter the repo or exit on error
+echo "Cloning complete, entering repo directory"
+cd repo || exit
 
-  # Kill everything and start the script
-  kill -- -1
+ # Kill everything and start the script
+kill -- -1
 
-  # Change permissions on start script
-  chmod +x ./start.sh
-  ./start.sh &
-done
+# Change permissions on start script
+chmod +x ./start.sh
+./start.sh
